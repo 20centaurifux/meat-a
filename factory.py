27463b79406cca6fba391bf0503685afca5c4a4b
lordkefir <lord-kefir@arcor.de>
2013-03-14 16:38:43 +0100

    implementation of new factory function to create ObjectDb instances

--- factory.py
+++ factory.py
@@ -2,3 +2,6 @@ import config, mongodb
 
 def create_user_db():
 	return mongodb.MongoUserDb(config.MONGODB_DATABASE, config.MONGODB_HOST, config.MONGODB_PORT)
+
+def create_object_db():
+	return mongodb.MongoObjectDb(config.MONGODB_DATABASE, config.MONGODB_HOST, config.MONGODB_PORT)
