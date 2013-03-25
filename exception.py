450218bf981d419bd86828f9db267a19f882354d
lordkefir <lord-kefir@arcor.de>
2013-03-25 16:04:14 +0100

    added exception classes & error codes

---
+++ exception.py
@@ -0,0 +1,29 @@
+import util
+
+ErrorCode = util.enum(INVALID_PARAMETER = 1,
+                      USER_ALREADY_EXISTS = 100,
+                      USERNAME_ALREADY_REQUESTED = 101,
+                      EMAIL_ALREADY_ASSIGNED = 102)
+
+class Exception:
+	def __init__(self, code, message):
+		self.code = code
+		self.message = message
+
+class InvalidParameterException(Exception):
+	def __init__(self, parameter):
+		Exception.__init__(self, ErrorCode.INVALID_PARAMETER, "Invalid parameter.")
+		self.parameter = parameter
+
+class UserAlreadyExistsException(Exception):
+	def __init__(self):
+		Exception.__init__(self, ErrorCode.USER_ALREADY_EXISTS, "The given username does already exist.")
+
+
+class UsernameAlreadyRequestedException(Exception):
+	def __init__(self):
+		Exception.__init__(self, ErrorCode.USERNAME_ALREADY_REQUESTED, "The given username has already been requested.")
+
+class EmailAlreadyAssignedException(Exception):
+	def __init__(self):
+		Exception.__init__(self, ErrorCode.EMAIL_ALREADY_ASSIGNED, "The given email address is already in use.")
