55742879a4409a5ae93119cc3c13bfe62e1ec4c6
lordkefir <lord-kefir@arcor.de>
2013-03-25 16:05:08 +0100

    added Application class & method to request user accounts

---
+++ app.py
@@ -0,0 +1,36 @@
+import factory, exception, util
+from validators import *
+from base64 import b64encode
+
+class Application:
+	def request_account(self, username, email):
+		# validate parameters:
+		if not validate_username(username):
+			raise exception.InvalidParameterException("username")
+
+		if not validate_email(email):
+			raise exception.InvalidParameterException("email")
+
+		# connect to database:
+		db = factory.create_user_db()
+
+		# test if user request, account or email already exist:
+		if db.username_requested(username):
+			raise exception.UsernameAlreadyRequestedException()
+
+		if db.user_exists(username):
+			raise exception.UserAlreadyExistsException()
+
+		if db.email_assigned(email):
+			raise exception.EmailAlreadyAssignedException()
+
+		# create activation code:
+		code = b64encode(util.generate_junk(128))
+
+		while db.user_request_code_exists(code):
+			code = b64encode(util.generate_junk(128))
+
+		# save user request:
+		db.create_user_request(username, email, code)
+
+		return code
