dcb5b8a1c7cc5a362a847c300d748b0fee6f9461
lordkefir <lord-kefir@arcor.de>
2013-03-25 15:52:03 +0100

    added validator functions

---
+++ validators.py
@@ -0,0 +1,21 @@
+import re
+
+username_regex = re.compile("^\w[\w\-\.]{1,15}$", re.IGNORECASE | re.UNICODE)
+email_regex = re.compile("^[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{2,4}$", re.IGNORECASE | re.UNICODE)
+
+def validate_string(regex, value):
+	if value is None:
+		value = ""
+
+	value = value.strip()
+
+	if not regex.match(value) is None:
+		return True
+
+	return False
+
+def validate_username(username):
+	return validate_string(username_regex, username)
+
+def validate_email(email):
+	return validate_string(email_regex, email)
