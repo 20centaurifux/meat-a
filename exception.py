a7f0ccf221de988ebb94711fbb137a0a96d8fb63
Sebastian Fedrau <lord-kefir@arcor.de>
2013-03-27 07:02:50 +0100

    new exceptions & error codes

--- exception.py
+++ exception.py
@@ -4,9 +4,12 @@ import util
 
 ErrorCode = util.enum(INVALID_PARAMETER = 1,
                       USER_ALREADY_EXISTS = 100,
-                      USERNAME_ALREADY_REQUESTED = 101,
-                      EMAIL_ALREADY_ASSIGNED = 102,
-                      INVALID_REQUEST_CODE = 103)
+                      COULD_NOT_FIND_USER = 101,
+                      USER_IS_BLOCKED = 102,
+                      USERNAME_ALREADY_REQUESTED = 103,
+                      EMAIL_ALREADY_ASSIGNED = 104,
+                      INVALID_REQUEST_CODE = 105,
+                      INVALID_PASSWORD = 106)
 
 class Exception:
 	def __init__(self, code, message):
@@ -22,6 +25,13 @@ class UserAlreadyExistsException(Exception):
 	def __init__(self):
 		Exception.__init__(self, ErrorCode.USER_ALREADY_EXISTS, "The given username does already exist.")
 
+class UserIsBlockedException(Exception):
+	def __init__(self):
+		Exception.__init__(self, ErrorCode.USER_IS_BLOCKED, "User is blocked.")
+
+class UserNotFoundException(Exception):
+	def __init__(self):
+		Exception.__init__(self, ErrorCode.COULD_NOT_FIND_USER, "Username not found.")
 
 class UsernameAlreadyRequestedException(Exception):
 	def __init__(self):
@@ -34,3 +44,7 @@ class EmailAlreadyAssignedException(Exception):
 class InvalidRequestCodeException(Exception):
 	def __init__(self):
 		Exception.__init__(self, ErrorCode.INVALID_REQUEST_CODE, "Invalid request code.")
+
+class InvalidPasswordException(Exception):
+	def __init__(self):
+		Exception.__init__(self, ErrorCode.INVALID_PASSWORD, "Invalid password.")
