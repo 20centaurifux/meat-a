48fd3c5fa73b3735d91b65ee71aaf3e46e934d7e
lordkefir <lord-kefir@arcor.de>
2013-05-03 07:09:46 +0200

    added new configuration values for request counting

--- config.py
+++ config.py
@@ -28,33 +28,37 @@
 	empty folders or remove empty folders on the remote site.
 """
 
-WEBSITE_URL              = "http://localhost:8000"
+WEBSITE_URL               = "http://localhost:8000"
 
-TEMPLATE_DIR             = "tpl"
+TEMPLATE_DIR              = "tpl"
 
-MONGODB_HOST             = "127.0.0.1"
-MONGODB_PORT             = 27017
-MONGODB_DATABASE         = "meata"
+MONGODB_HOST              = "127.0.0.1"
+MONGODB_PORT              = 27017
+MONGODB_DATABASE          = "meata"
 
-LANGUAGES                = [ "en", "de" ]
+LANGUAGES                 = [ "en", "de" ]
 
-REQUEST_EXPIRY_TIME      = 60
+REQUEST_EXPIRY_TIME       = 60
 
-USER_REQUEST_TIMEOUT     = 600
-PASSWORD_REQUEST_TIMEOUT = 600
-PASSWORD_RESET_TIMEOUT   = 600
-DEFAULT_EMAIL_LIFETIME   = 7200
-REQUEST_CODE_LENGTH      = 128
-DEFAULT_PASSWORD_LENGTH  = 8
+USER_REQUEST_TIMEOUT      = 600
+PASSWORD_REQUEST_TIMEOUT  = 600
+PASSWORD_RESET_TIMEOUT    = 600
+DEFAULT_EMAIL_LIFETIME    = 7200
+REQUEST_CODE_LENGTH       = 128
+DEFAULT_PASSWORD_LENGTH   = 8
 
-UPLOAD_DIR               = "upload"
-TMP_DIR                  = "tmp"
+UPLOAD_DIR                = "upload"
+TMP_DIR                   = "tmp"
 
-AVATAR_MAX_FILESIZE      = 1024 * 1024
-AVATAR_MAX_WIDTH         = 180
-AVATAR_MAX_HEIGHT        = 180
-AVATAR_FORMATS           = [ "JPEG", "PNG", "GIF" ]
-AVATAR_EXTENSIONS        = [ ".jpg", ".jpeg", ".png", ".gif" ]
-AVATAR_DIR               = "images\users"
+AVATAR_MAX_FILESIZE       = 1024 * 1024
+AVATAR_MAX_WIDTH          = 180
+AVATAR_MAX_HEIGHT         = 180
+AVATAR_FORMATS            = [ "JPEG", "PNG", "GIF" ]
+AVATAR_EXTENSIONS         = [ ".jpg", ".jpeg", ".png", ".gif" ]
+AVATAR_DIR                = "images\users"
 
-WSGI_MAX_REQUEST_LENGTH  = 1024 * 1024
+WSGI_MAX_REQUEST_LENGTH   = 1024 * 1024
+
+ACCOUNT_REQUESTS_PER_HOUR = 15
+PASSWORD_RESETS_PER_HOUR  = 5
+REQUESTS_PER_HOUR         = 1800
