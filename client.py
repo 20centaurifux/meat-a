619cd8eee65c6e97c030c4f4c55d630d394b31d3
Sebastian Fedrau <lord-kefir@arcor.de>
2013-05-16 18:31:41 +0200

    code documentation (client module)

--- client.py
+++ client.py
@@ -24,29 +24,51 @@
 	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 	OTHER DEALINGS IN THE SOFTWARE.
 """
+##
+#  @file client.py
+#  An example client.
+
+## @package client
+#  An example client.
 
 import urllib, urllib2, json, util, os, itertools, mimetools, mimetypes
 
+## Class used to create multi-part forms.
 class MultiPartForm:
 	def __init__(self):
+		## Fields stored in the form.	
 		self.form_fields = []
+		## Files stored in the form.
 		self.files = []
+		## The boundary.	
 		self.boundary = mimetools.choose_boundary()
 
+	## Gets content type of the form.
+	#  @return a string
 	def get_content_type(self):
 		return "multipart/form-data; boundary=%s" % self.boundary
 
+	## Adds a new field to the form.
+	#  @param name name of the field
+	#  @param value value of the field
 	def add_field(self, name, value):
 		self.form_fields.append((name, value))
 
-	def add_file(self, fieldname, filename, fileHandle, mimetype=  None):
-		body = fileHandle.read()
+	## Adds a file to the form.
+	#  @param fieldname name of the field
+	#  @param filename name of the file to add
+	#  @param file_handle handler used to read file content
+	#  @param mimetype content type of the file
+	def add_file(self, fieldname, filename, file_handle, mimetype=None):
+		body = file_handle.read()
 
 		if mimetype is None:
 			mimetype = mimetypes.guess_type(filename)[0] or "application/octet-stream"
 
 		self.files.append((fieldname, filename, mimetype, body))
 
+	## Converts the multi-part form to a string.
+	#  @return a string
 	def __str__(self):
 		parts = []
 		part_boundary = "--" + self.boundary
@@ -61,11 +83,19 @@ class MultiPartForm:
 
 		return "\r\n".join(flattened)
 
+## Example client.
 class Client:
+	## The constructor.
+	#  @param url url of the webservice
+	#  @param port port of the webservice
 	def __init__(self, url, port):
 		self.url = url
 		self.port = port
 
+	## Requests a user account.
+	#  @param username requested username
+	#  @param email email address of the account
+	#  @return webservice response
 	def request_account(self, username, email):
 		params = { "username": username, "email": email }
 		data = urllib.urlencode(params)
@@ -74,25 +104,59 @@ class Client:
 
 		return urllib2.urlopen(req).read()
 
+	## Disables a user account.
+	#  @param username user to disable
+	#  @param password user password (plaintext)
+	#  @param email email address of the user
+	#  @return webservice response
 	def disable_user(self, username, password, email):
 		return self.__post__("/account/disable", password, username = username, email = email)
 
+	## Requests a new password.
+	#  @param username a username
+	#  @param email email address of the user
+	#  @return webservice response
 	def request_password(self, username, email):
 		params = { "username": username, "email": email }
 		url = "%s%s" % (self.__build_url__("/account/password/request?"), urllib.urlencode(params))
 
 		return self.get(url)
 
+	## Updates a user password.
+	#  @param username a username
+	#  @param old_password password of the account (plaintext)
+	#  @param new_password new password to set (plaintext)
+	#  @return webservice response
 	def update_password(self, username, old_password, new_password):
 		return self.__post__("/account/password/update", old_password, username = username, old_password = old_password, new_password = new_password)
 
+	## Updates user details.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param email email address to set
+	#  @param firstname firstname to set
+	#  @param lastname lastname to set
+	#  @param gender gender to set
+	#  @param language language to set
+	#  @param protected protected status to set
+	#  @return webservice response
 	def update_user_details(self, username, password, email, firstname, lastname, gender, language, protected):
 		return self.__post__("/account/update", password, username = username, email = email, firstname = firstname, lastname = lastname,
 		                     gender = gender, language = language, protected = protected)
 
+	## Gets details of a user account.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param name name of an existing user account
+	#  @return webservice response
 	def get_user_details(self, username, password, name):
 		return self.__post__("/account/details", password, username = username, name = name)
 
+	## Updates an avatar.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param filename name of a file to upload
+	#  @return webservice response
 	def update_avatar(self, username, password, filename):
 		form = MultiPartForm()
 		t = util.unix_timestamp()
@@ -115,27 +179,73 @@ class Client:
 
 		return urllib2.urlopen(req).read()
 
+	## Gets favorites of a user.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param page page number
+	#  @param page_size size of each page
+	#  @return webservice response
 	def get_favorites(self, username, password, page, page_size):
 		return self.__post__("/account/favorites", password, username = username, page = page, page_size = page_size)
 
+	## Searches the user data store.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param query a search query
+	#  @return webservice response
 	def find_user(self, username, password, query):
 		return self.__post__("/account/search", password, username = username, query = query)
 
+	## Gets object details.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param guid guid of an object.
+	#  @return webservice response
 	def get_object(self, username, password, guid):
 		return self.__post__("/object/details", password, username = username, guid = guid)
 
+	## Gets objects.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param page page number
+	#  @param page_size size of each page
+	#  @return webservice response
 	def get_objects(self, username, password, page, page_size):
 		return self.__post__("/objects", password, username = username, page = page, page_size = page_size)
 
+	## Gets objects assigned to an tag.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param tag a tag
+	#  @param page page number
+	#  @param page_size size of each page
+	#  @return webservice response
 	def get_tagged_objects(self, username, password, tag, page, page_size):
 		return self.__post__("/objects/tag", password, username = username, tag = tag, page = page, page_size = page_size)
 
+	## Gets random objects.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param page_size the page size
+	#  @return webservice response
 	def get_random_objects(self, username, password, page_size):
 		return self.__post__("/objects/random", password, username = username, page_size = page_size)
 
+	## Gets most popular objects.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param page page number
+	#  @param page_size size of each page
+	#  @return webservice response
 	def get_popular_objects(self, username, password, page, page_size):
 		return self.__post__("/objects/popular", password, username = username, page = page, page_size = page_size)
 
+	## Adds tags to an object.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param guid guid of an object
+	#  @param tags array containing tags
+	#  @return webservice response
 	def add_tags(self, username, password, guid, tags):
 		t = util.unix_timestamp()
 		sign = util.sign_message(util.hash(password), username = username, timestamp = t, guid = guid, tags = tags)
@@ -146,21 +256,58 @@ class Client:
 
 		return res.read()
 
+	## Upvotes/Downvotes an object.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param guid guid of an object
+	#  @param up True to upvote
+	#  @return webservice response
 	def rate(self, username, password, guid, up = True):
 		return self.__post__("/object/rate", password, username = username, guid = guid, up = str(up).lower())
 
+	## Adds an object to the favorites list of a user.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param guid guid of an object
+	#  @param favor True to add the object to the favorites list
+	#  @return webservice response
 	def favor(self, username, password, guid, favor = True):
 		return self.__post__("/object/favor", password, username = username, guid = guid, favor = str(favor).lower())
 
+	## Adds a comment to an object.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param guid guid of an object
+	#  @param text text to append
+	#  @return webservice response
 	def add_comment(self, username, password, guid, text):
 		return self.__post__("/object/comments/add", password, username = username, guid = guid, text = text)
 
+	## Gets comments assigned to an object.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param guid guid of an object
+	#  @param page page number
+	#  @param page_size size of each page
+	#  @return webservice response
 	def get_comments(self, username, password, guid, page, page_size):
 		return self.__post__("/object/comments", password, username = username, guid = guid, page = page, page_size = page_size)
 
+	## Lets a user follow another user.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param user name of a user account
+	#  @param follow True to follow a user
+	#  @return webservice response
 	def follow(self, username, password, user, follow = True):
 		return self.__post__("/account/follow", password, username = username, user = user, follow = str(follow).lower())
 
+	## Recommends an object to friends.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param guid guid of an object
+	#  @param receivers array holding receivers
+	#  @return webservice response
 	def recommend(self, username, password, guid, receivers):
 		t = util.unix_timestamp()
 		sign = util.sign_message(util.hash(password), username = username, timestamp = t, guid = guid, receivers = receivers)
@@ -171,15 +318,30 @@ class Client:
 
 		return res.read()
 
+	## Gets objects recommended to a user.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param page page number
+	#  @param page_size size of each page
+	#  @return webservice response
 	def get_recommendations(self, username, password, page, page_size):
 		return self.__post__("/account/recommendations", password, username = username, page = page, page_size = page_size)
 
+	## Gets messages sent to a user.
+	#  @param username a username
+	#  @param password user password (plaintext)
+	#  @param limit maximum number of messages to receive
+	#  @param older_than filter to get only messages older than the specified timestamp
+	#  @return webservice response
 	def get_messages(self, username, password, limit, older_than):
 		if older_than is None:
 			older_than = "null"
 
 		return self.__post__("/account/messages", password, username = username, limit = limit, older_than = older_than)
 
+	## Sends a GET request to a server.
+	#  @param url an URL
+	#  @return response of the server
 	def get(self, url):
 		req = urllib2.Request(url)
 
