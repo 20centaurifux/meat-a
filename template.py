11f17554d98c20d4dbad76d44b5aabb93460867f
Sebastian Fedrau <lord-kefir@arcor.de>
2013-05-16 07:42:52 +0200

    code documentation (template module)

--- template.py
+++ template.py
@@ -27,11 +27,22 @@
 	This synchronziation procedure works only file-based. It will not upload
 	empty folders or remove empty folders on the remote site.
 """
+##
+#  @file template.py
+#  Classes used for building strings.
+
+## @package template
+#  Classes used for building strings.
 
 import abc, os, config, Cheetah.Template
 
+## Dictionary storing content of template files.
 tpl_cache = {}
 
+## Loads the content from a template file and stores it in the template cache. Returns the
+#  cached content whenever possible.
+#  @param filename filename of the template to load
+#  @return content of the template file
 def load_template_def(filename):
 	global tpl_cache
 
@@ -44,14 +55,22 @@ def load_template_def(filename):
 
 	return tpl
 
+## Base class for templates. A template can be exported to a string which contains binded data.
 class Template:
+	## Binds data.
+	#  @param kwargs data to bind
 	@abc.abstractmethod
-	def bind(self): return
+	def bind(self, **kwargs): return
 
+	## Converts the template to a string.
+	#  @return a string
 	@abc.abstractmethod
 	def render(self): return None
 
+## A Template implementation using the Cheetah framework.
 class CheetahTemplate(Template):
+	## The constructor.
+	#  @param tpl name of a Cheetah template file
 	def __init__(self, tpl):
 		self.__namespace = {}
 		self.__def = None
@@ -68,7 +87,11 @@ class CheetahTemplate(Template):
 
 		return str(t)
 
+## A Template implementation using the Cheeta framework. It renders body and subject of a mail.
 class CheetahMailTemplate(Template):
+	## The constructor.
+	#  @param subject_tpl template file used to generate the subject
+	#  @param body_tpl template file used to generate the body
 	def __init__(self, subject_tpl, body_tpl):
 		self.__namespace = {}
 		self.__defs = None
@@ -78,6 +101,8 @@ class CheetahMailTemplate(Template):
 	def bind(self, **kwargs):
 		self.__namespace = kwargs
 
+	## Converts subject and body template to a string.
+	#  @return subject and body
 	def render(self):
 		if self.__defs is None:
 			self.__defs = []
@@ -89,34 +114,42 @@ class CheetahMailTemplate(Template):
 
 		return str(subject), str(body)
 
+## A Template implementation used for sending account request codes.
 class AccountRequestMail(CheetahMailTemplate):
 	def __init__(self):
 		CheetahMailTemplate.__init__(self, "account_request_subject.tpl", "account_request_body.tpl")
 
+## A Template implementation used to welcome new users.
 class AccountActivationMail(CheetahMailTemplate):
 	def __init__(self):
 		CheetahMailTemplate.__init__(self, "account_activation_subject.tpl", "account_activation_body.tpl")
 
+## A Template implementation used to inform users that their account has been disabled.
 class AccountDisabledMail(CheetahMailTemplate):
 	def __init__(self):
 		CheetahMailTemplate.__init__(self, "account_disabled_subject.tpl", "account_disabled_body.tpl")
 
+## A Template implementation used for sending password request codes.
 class RequestNewPasswordMail(CheetahMailTemplate):
 	def __init__(self):
 		CheetahMailTemplate.__init__(self, "password_request_subject.tpl", "password_request_body.tpl")
 
+## A Template implementation used to send a new generated password to users.
 class PasswordResetMail(CheetahMailTemplate):
 	def __init__(self):
 		CheetahMailTemplate.__init__(self, "password_reset_subject.tpl", "password_reset_body.tpl")
 
+## Website to inform users that their account has been activated.
 class AccountActivatedPage(CheetahTemplate):
 	def __init__(self):
 		CheetahTemplate.__init__(self, "account_activated_html.tpl")
 
+## Website to display faliure messages.
 class FailureMessagePage(CheetahTemplate):
 	def __init__(self):
 		CheetahTemplate.__init__(self, "failure_message_html.tpl")
 
+## Website to inform users that their password has been reseted.
 class PasswordResetPage(CheetahTemplate):
 	def __init__(self):
 		CheetahTemplate.__init__(self, "password_reset_html.tpl")
