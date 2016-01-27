8161a3889e000cee930a19edc57a43ac3cd4d9ae
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2016-01-27 07:49:21 +0100

    removed obsolete stuff in util.py

--- util.py
+++ util.py
@@ -34,10 +34,10 @@
 
 from time import mktime
 from datetime import datetime
-from hashlib import sha1, sha256
+from hashlib import sha256
 from bson import json_util
 from urllib2 import quote
-import random, string, json, os, hmac, uuid
+import random, string, json, uuid
 
 ## Gets the current timestamp (UTC) in milliseconds.
 #  @return a float
@@ -45,13 +45,6 @@ def now():
 	now = datetime.utcnow()
 
 	return mktime(now.timetuple()) * 1000 + now.microsecond / 1000
-
-## Gets the current UNIX timestamp (UTC).
-#  @return an integer
-def unix_timestamp():
-	now = datetime.utcnow()
-
-	return int(mktime(now.timetuple()))
 
 ## Calculates the checksum of a string.
 #  @param plain a string
@@ -72,68 +65,9 @@ def password_hash(plain, salt):
 
 	return m.hexdigest()
 
-## Calculates the checksum of a file.
-#  @param filename name of a file
-#  @param hasher hash algorithm (e.g. hashlib.md5) to calculate checksum
-#  @param block_size size of blocks read from file
-#  @return checksum as hex-string
-def hash_file(filename, hasher, block_size = 81920):
-	stream = open(filename, "rb")
-
-	for bytes in read_from_stream(stream, block_size):
-		hasher.update(bytes)
-
-	stream.close()
-
-	return hasher.hexdigest()
-
 ## Generates a guid.
 def new_guid():
 	return str(uuid.uuid4())
-
-## Calculates the checksum of multiple arguments. At first the parameters will be
-#  sorted alphabetically. Then the HMAC-SHA1 checksum will be calculated using the
-#  given secret.
-#  @param secret secret used to calculate checksum
-#  @param kwargs arguments
-#  @return checksum as hex-string
-def sign_message(secret, **kwargs):
-	def serialize(value):
-		t = type(value)
-
-		if value is None:
-			return "null"
-		if t is int or t is long:
-			return str(value)
-		elif t is str:
-			return value
-		elif t is unicode:
-			return value.encode("utf-8")
-		elif t is bool:
-			return str(value).lower()
-		elif t is float:
-			return str("%.2f" % value)
-		else:
-			raise Exception("Invalid parameter type: %s" % type(value))
-
-	if type(secret) is unicode:
-		secret = secret.encode("utf-8")
-
-	h = hmac.new(secret, "", sha1)
-
-	update = h.update
-
-	for key in sorted(kwargs.keys(), key = lambda k: k.upper()):
-		obj = kwargs[key]
-
-		t = type(obj)
-
-		if t is list or t is tuple:
-			map(update, (serialize(v) for v in obj))
-		else:
-			update(serialize(obj))
-
-	return h.hexdigest()
 
 ## Generates a random string.
 #  @param length length of the generated string
@@ -201,13 +135,6 @@ def to_bool(obj):
 #  @return an url with quoted parameters
 def build_url(fmt, url, *params):
 	return url + fmt % tuple(map(quote, params))
-
-## Removes all files found in a directory.
-#  @param directory path to a directory
-def remove_all_files(directory):
-	for file in os.listdir(directory):
-		path = os.path.join(directory, file)
-		os.remove(path)
 
 ## Generator to read blocks from an input stream.
 #  @param stream input stream
