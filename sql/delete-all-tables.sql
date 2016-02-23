146aee8a057504e250a0610e6b05d61152ebb809
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2016-02-23 16:22:19 +0100

    added script to delete all tables

---
+++ sql/delete-all-tables.sql
@@ -0,0 +1,13 @@
+﻿delete from public_message
+delete from user_favorite
+delete from user_recommendation
+delete from object_score
+delete from object_tag
+delete from object_comment
+delete from object
+delete from password_request
+delete from user_friendship
+delete from user_request
+delete from mail
+delete from message
+delete from "user"
